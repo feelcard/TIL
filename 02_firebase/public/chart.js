85b37a0f910b0bce0a70ab802deb17c1f5aaf1ee
feelcard <yhsyhs121@gmail.com>
2020-01-01 01:00:50 +0900

    firebase learn how to use query

--- 02_firebase/public/chart.js
+++ 02_firebase/public/chart.js
@@ -2,20 +2,32 @@
 
 var db = firebase.firestore();
 
-var collection = "";
-var doc="";
 
-function convertDate(timestamp){
+var startDate;
+var endDate;
+
+db.collection("study").where("time",">=",new Date('2019-11-30T00:00:00')).where("time","<=",new Date('2019-12-31T00:00:00')).get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+    });
+})
+.catch(function(error) {
+    console.log("Error getting documents: ", error);
+});
+
+function convertDate(timestamp) {
 
     return new firebase.firestore.Timestamp(timestamp.seconds, timestamp.nanoseconds).toDate()
 }
 
 db.collection("study").doc("test1").get().then(function (doc) {
     var stat = doc.data().status;
-    var realtime= new Date();
+    var realtime = new Date();
+  
     console.log(convertDate(stat.time).getMonth());
-    
- 
+
+
     window.chartColors = {
         red: 'rgb(255, 99, 132)',
         orange: 'rgb(255, 159, 64)',
@@ -25,11 +37,11 @@ db.collection("study").doc("test1").get().then(function (doc) {
         purple: 'rgb(153, 102, 255)',
         grey: 'rgb(201, 203, 207)'
     };
-    var MONTHS = [convertDate(stat.time).getMonth()+1+"월", convertDate(stat.time).getDate()+"일",convertDate(stat.time).getHours()+"시" , convertDate(stat.time).getMinutes()+"분", 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var MONTHS = [convertDate(stat.time).getMonth() + 1 + "월", convertDate(stat.time).getDate() + "일", convertDate(stat.time).getHours() + "시", convertDate(stat.time).getMinutes() + "분", 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     var config = {
         type: 'line',
         data: {
-            labels: [convertDate(stat.time).getMonth()+1+"월", convertDate(stat.time).getDate()+"일",convertDate(stat.time).getHours()+"시" , convertDate(stat.time).getMinutes()+"분", 'May', 'June', 'July'],
+            labels: [convertDate(stat.time).getMonth() + 1 + "월", convertDate(stat.time).getDate() + "일", convertDate(stat.time).getHours() + "시", convertDate(stat.time).getMinutes() + "분", 'May', 'June', 'July'],
             datasets: [{
                 label: 'My First dataset',
                 backgroundColor: window.chartColors.red,
@@ -102,7 +114,7 @@ db.collection("study").doc("test1").get().then(function (doc) {
 
         config.data.datasets.push(newDataset);
         window.myLine.update();
-    });
+    }); 
 
     document.getElementById('addData').addEventListener('click', function () {
         if (config.data.datasets.length > 0) {
@@ -130,19 +142,20 @@ db.collection("study").doc("test1").get().then(function (doc) {
         });
 
         window.myLine.update();
-    });
+    });  
 
     document.getElementById('addStatus').addEventListener('click', function () {
         config.data.labels.splice(-1, 1); // remove the label first
-
+        console.log("data input");
         db.collection("study").doc(realtime.toString()).set({
-            status: {
-                temp: Math.floor(Math.random() * 10) + 1,
-                hum: Math.floor(Math.random() * 10) + 1,
-                time: realtime
-            }
+
+            temp: Math.floor(Math.random() * 10) + 1,
+            hum: Math.floor(Math.random() * 10) + 1,
+            time: realtime
+
         })
             .then(function () {
+              
                 console.log("Document successfully written!");
             })
             .catch(function (error) {
