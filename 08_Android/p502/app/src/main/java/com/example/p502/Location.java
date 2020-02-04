9a0283521c8475fec7d01ccdb76bbb363e034a77
feelcard <yhsyhs121@gmail.com>
2020-02-04 20:51:32 +0900

    Android Map Learn

---
+++ 08_Android/p502/app/src/main/java/com/example/p502/Location.java
@@ -0,0 +1,48 @@
+package com.example.p502;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Location implements Parcelable {
+    double leti;
+    double longti;
+
+    public Location(){
+
+    }
+
+    public Location(Parcel src){
+    this.leti = src.readDouble();
+    this.longti = src.readDouble();
+    }
+    public Location(double leti, double longti) {
+        this.leti = leti;
+        this.longti = longti;
+    }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        @Override
+        public Location createFromParcel(Parcel source) {
+            return new Location(source);
+        }
+
+        @Override
+        public Object[] newArray(int size) {
+            return new Location[size];
+        }
+    };
+
+    public void writeToParcel(Parcel dest, int flags){
+        dest.writeDouble(leti);
+        dest.writeDouble(longti);
+    }
+
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+
+}
---
+++ 08_Android/p502/app/src/main/java/com/example/p502/ThirdActivity.java
@@ -0,0 +1,17 @@
+package com.example.p502;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.MapView;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+    }
+}
